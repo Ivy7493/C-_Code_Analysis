83b128339c281bc5b4d030d9bf4bac1381d418b0
Ivan Von Staden <ivanblizz23@gmail.com>
2022-09-19 10:12:01 +0200

    Set up inital structure of the program, Plugin Point Established

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
 
-from distutils import extension
-import os, sys #need this in order to read in infomation.
+#imports for different services
+from parserService import getFiles
 
-
-
-
-def getFiles():
-    # Get the list of all files and directories
-    path = os.path.dirname(__file__)
-    #dir_list = os.walk(path)
-    cppList = []
-    headerList = []
-    print("Files and directories in '", path, "' :")
-    for (root, dirs, file) in os.walk(path):
-        for f in file:
-            if '.cpp' in f:
-                cppList.append(f)
-                print(f)
-                sourceFile = os.path.join(root, f)
-                source = open(sourceFile, "r")
-                source_lines = []
-                code_block = False
-                for source_line in source:
-                    if('/*' in source_line):
-                          code_block = True
-                    if('*/' in source_line):
-                           code_block = False
-                    if( not code_block):
-                          source_lines.append(source_line.split('//')[0].strip()) # if not in code block, insert line with comments removed
-                            
-                print(source_lines)
-            elif '.h' in f:
-                headerList.append(f)
-                print(f)
-    print("header List: ", headerList)
-    print("cpp List: ", cppList)
-
-    return headerList,cppList
         
 
 
 
 
-getFiles();
+
+def main():
+    headers,source = getFiles()
+
+
+if __name__ == "__main__":
+    main()
---
+++ parserService.py
@@ -0,0 +1,55 @@
+import os, sys #need this in order to read in infomation.
+def getFiles():
+    # Get the list of all files and directories
+    path = os.path.dirname(__file__)
+    #dir_list = os.walk(path)
+    cppList = {}
+    headerList = {}
+    print("Files and directories in '", path, "' :")
+    for (root, dirs, file) in os.walk(path):
+        for f in file:
+            if '.cpp' in f:
+                print(f)
+                sourceFile = os.path.join(root, f)
+                source = open(sourceFile, "r")
+                source_lines = []
+                code_block = False
+                for source_line in source:
+                    if('/*' in source_line):
+                          code_block = True
+                    if('*/' in source_line):
+                           code_block = False
+                    if( not code_block):
+                          source_lines.append(source_line.split('//')[0].strip()) # if not in code block, insert line with comments removed         
+                print(source_lines)
+                cppList[f]=source_lines
+                
+            elif '.h' in f:
+                print(f)
+                sourceFile = os.path.join(root, f)
+                source = open(sourceFile, "r")
+                source_lines = []
+                code_block = False
+                for source_line in source:
+                    if('/*' in source_line):
+                          code_block = True
+                    if('*/' in source_line):
+                           code_block = False
+                    if( not code_block):
+                          source_lines.append(source_line.split('//')[0].strip()) # if not in code block, insert line with comments removed         
+                print(source_lines)
+                headerList[f]=source_lines
+    
+    for x in cppList:
+        print(" ")
+        print("cpp Item: ", x)
+        for y in cppList[x]:
+            print(y)
+
+    for x in headerList:
+        print(" ")
+        print("header Item: ", x)
+        for y in headerList[x]:
+            print(y)
+
+    return headerList,cppList
