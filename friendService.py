6358c8b821b6c6d94f14120ce231cd4462c5d2b2
Ivan Von Staden <ivanblizz23@gmail.com>
2022-09-26 12:59:54 +0200

    Added basic unit test structure

--- friendService.py
+++ friendService.py
@@ -4,7 +4,7 @@ def analyzeFriend(file):
     locationOccuration = []
     currentLine = 0
     for x in file:
-        if ("friend" in x) and ('()' in x) and (not x == ""): #maybe not include () because a friend doesnt have to be a function but a friend is a friend
+        if ("friend" in x) and ('()'in x) and (not x == ""): #maybe not include () because a friend doesnt have to be a function but a friend is a friend
             friendCount = friendCount + 1 #inc in the case where we find the friend keyword
             locationOccuration.append(currentLine)
         currentLine = currentLine + 1 #inc in order to see where the occurance of the friend occures
