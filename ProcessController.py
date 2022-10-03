7c8bfe8bcb0e16fc0fe6de7410b413d854d15907
Ivan Von Staden <ivanblizz23@gmail.com>
2022-10-03 09:59:42 +0200

    Added raw file line locator

--- ProcessController.py
+++ ProcessController.py
@@ -1,5 +1,5 @@
 #imports for different services
-from parserService import getFiles
+from parserService import getFiles,findRawLocation
 from friendService import analyzeFriend
 from globalService import analyzeGlobalVariables
 from switchService import analyzeSwitch
@@ -8,7 +8,7 @@ from implementationInheritanceService import analyzeImplementationInheritance
 from dryService import analyzeDRY
 
 def ProcessController(fileName):
-    headers,source = getFiles(fileName)
+    headers,source,rawHeaders,rawSource = getFiles(fileName)
     locationOccurrencesForImplementationInheritance = []
     GlobalImplementationInheritanceCount = 0
     locationOccurrencesForPublic = []
@@ -95,6 +95,16 @@ def ProcessController(fileName):
         for member in fileSwitchLocation:
             locationOccurrencesForSwitch.append(x + '-' + str(member))
         GlobalSwitchCount += fileSwitchCount
+
+
+    #======================Raw Location Test===============================#
+    rawGlobalLocations = findRawLocation(locationOccurencesForGlobal,rawHeaders,rawSource,source,headers)
+    rawSwitchLocations = findRawLocation(locationOccurrencesForSwitch,rawHeaders,rawSource,source,headers)
+    rawFriendLocations = findRawLocation(locationOccurrencesForFriend,rawHeaders,rawSource,source,headers)
+    rawPublicLocations = findRawLocation(locationOccurrencesForPublic,rawHeaders,rawSource,source,headers)
+    rawInheritanceLocations = findRawLocation(locationOccurrencesForImplementationInheritance,rawHeaders,rawSource,source,headers)
+    print("====================HERE=====================")
+    print(list(set(rawInheritanceLocations)))
 
     #-----------------------TOTAL SECTION--------------------------------#
     issueLocationArr = [list(set(locationOccurrencesForImplementationInheritance)),locationOccurencesForGlobal,list(set(locationOccurrencesForPublic)),list(set(locationOccurrencesForSwitch)),list(set(locationOccurrencesForFriend))]
