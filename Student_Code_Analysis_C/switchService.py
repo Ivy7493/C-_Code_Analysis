4780d1ddf6601640f8c0880258d58bf1458bffc5
Ivan Von Staden <ivanblizz23@gmail.com>
2022-10-12 10:45:54 +0200

    Added indents, Added Colour Change, Fully intergrated DRY to front-end, switch member now highlights all

--- Student_Code_Analysis_C/switchService.py
+++ Student_Code_Analysis_C/switchService.py
@@ -1,12 +1,16 @@
 
-
 def analyzeSwitch(file):
     locationOccuration = [];
     currentLine = 0;
 
     for line in file:
         if("switch" in line and ("(" in line or ")" in line) and ("int" not in line and "double" not in line and "string" not in line and "auto" not in line and "char" not in line and "bool" not in line and "float" not in line) and ('=' not in line)):
-            locationOccuration.append(currentLine)
+            #locationOccuration.append(currentLine)
+            tempCounter = currentLine;
+            startBlock = currentLine;
+            while( '}' not in file[tempCounter]):
+                tempCounter += 1
+            locationOccuration.append(str(startBlock) + '@' + str(tempCounter))
         currentLine = currentLine + 1
     return locationOccuration
 
